ef09957dbaf882f6f8ebb26ce384ba08b13f5027
Keenan <kkuklisatunhla@gmail.com>
2026-04-11 16:04:53 +1000

    silver orderbook

---
+++ models/silver/polymarket_orderbook.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='table')}}
+
+WITH source AS (
+    SELECT 
+        data
+    FROM 
+        {{ source('bronze', 'polymarket_orderbook')}}
+)
+
+SELECT 
+    data:id::varchar AS id,
+    data:maker::varchar AS maker,
+    data:taker::varchar AS taker,
+    data:makerAssetId::varchar AS maker_asset_id,
+    data:takerAssetId::varchar AS taker_asset_id,
+    data:makerAmountFilled::float AS maker_amount_filled,
+    data:takerAmountFilled::float AS taker_amount_filled,
+    data:fee::float AS fee,
+    data:timestamp::timestamp_ntz AS timestamp,
+    data:transactionHash::varchar AS transaction_hash
+FROM
+    source 
+WHERE 
+    data:id IS NOT NULL
